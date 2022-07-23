89198098dbf3a03790ba333e9ee5984e29548f97
Tanbir Banipal <tbanipal@uwaterloo.ca>
2022-07-22 20:52:37 -0400

    Resolve potential error regarding moves

--- types.h
+++ types.h
@@ -12,11 +12,11 @@ enum PieceType{
     Empty = 0 // ChessPiece should not have this type. Cell.getType() should return this type though
 };
 
-enum locationx{A,B,C,D,E,F,G,H};
+enum locationx{A = 1,B = 2,C = 3,D = 4,E = 5,F = 6,G = 7,H = 8};
 
 typedef std::pair<PieceType, bool> Piece;
 typedef std::pair<locationx, int> position;
-typedef std::pair<position, position> move;
-typedef std::pair<move, PieceType> moveinfo;
+typedef std::pair<position, position> chessmove;
+typedef std::pair<chessmove, PieceType> moveinfo;
 
 #endif
