6a3cfb5f69774137e202b07f86c7272aa639ae25
Tanbir Banipal <tbanipal@uwaterloo.ca>
2022-07-22 20:25:36 -0400

    Add new type

--- types.h
+++ types.h
@@ -3,17 +3,18 @@
 #include <utility>
 
 enum PieceType{
-    Pawn,
-    Bishop,
-    Rook,
-    Knight,
-    Queen,
-    King
+    Pawn = 1,
+    Bishop = 2,
+    Rook = 3,
+    Knight = 4,
+    Queen = 5,
+    King = 6,
+    Empty = 0 // ChessPiece should not have this type. Cell.getType() should return this type though
 };
 
 enum locationx{A,B,C,D,E,F,G,H};
 
-
+typedef std::pair<PieceType, bool> Piece;
 typedef std::pair<locationx, int> position;
 typedef std::pair<position, position> move;
 typedef std::pair<move, PieceType> moveinfo;
